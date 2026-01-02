dfdcbf72cd97d567d0e8b8b2afa8d96015ae60f3
David <davidle90@hotmail.com>
2026-01-02 14:32:27 +0100

    add findOne user and add usermapper

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { UserMapper } from './mappers/user.mapper';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get()
-  async getUsers() {
-    const users = this.usersService.getAllUsers();
-    return users;
+  async findAll() {
+    const users = await this.usersService.getAllUsers();
+    return { data: UserMapper.toResponseList(users)};
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    const user = await this.usersService.findById(id);
+    if(!user) throw new HttpException('User not found', HttpStatus.NOT_FOUND)
+    return { data: UserMapper.toResponse(user)};
   }
 }
